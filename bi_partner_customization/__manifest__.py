60608e8ef5e4c2fed8081987dab073da8ed57e2d
laminemhedhbi <mohamed.amine.mhedhbi@gmail.com>
2022-01-22 15:27:28 +0100

    VM

--- bi_partner_customization/__manifest__.py
+++ bi_partner_customization/__manifest__.py
@@ -18,7 +18,7 @@
         "views/res_partner.xml",
         "views/medical_speciality.xml",
         "views/region.xml",
-        "views/potential.xml",
+        "views/department.xml",
         "views/brick_ims.xml",
     ],
 }
--- bi_partner_customization/models/res_partner.py
+++ bi_partner_customization/models/res_partner.py
@@ -8,7 +8,7 @@ class ResPartner(models.Model):
     medical_id = fields.Many2one("medical.speciality", string="Medical Speciality")
     region_id = fields.Many2one("geographical.region", string="Geographical Region")
     last_name = fields.Char("Last Name")
-    potential_id = fields.Many2one("potential ", string="Potential ")
+    department_id = fields.Many2one("department", string="Department")
     brick_id = fields.Many2one("brick.ims", string="Brick IMS")
     is_health = fields.Boolean("is_health", copy=False)
 
