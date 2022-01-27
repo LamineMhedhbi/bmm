cfa703f2999868d1e5f62da5bad39044600ba4d6
laminemhedhbi <mohamed.amine.mhedhbi@gmail.com>
2022-01-27 09:35:27 +0100

    Add Extra Fields

--- bi_partner_customization/models/res_partner.py
+++ bi_partner_customization/models/res_partner.py
@@ -9,6 +9,8 @@ class ResPartner(models.Model):
     region_id = fields.Many2one("geographical.region", string="Geographical Region")
     last_name = fields.Char("Last Name")
     department_id = fields.Many2one("department", string="Department")
+    potential_id = fields.Many2one("potential", string="Potential")
+    gsu_id = fields.integer("GSU")
     brick_id = fields.Many2one("brick.ims", string="Brick IMS")
     is_health = fields.Boolean("is_health", copy=False)
 
