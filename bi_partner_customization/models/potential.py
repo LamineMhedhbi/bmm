711d32a946ddd458b8415e1bfbef866e60f2d688
laminemhedhbi <mohamed.amine.mhedhbi@gmail.com>
2022-01-27 10:25:55 +0100

    Add Fields

--- bi_partner_customization/models/potential.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class Potential(models.Model):
-    _name = "potential"
-
-    _sql_constraints = [("internal_division", "UNIQUE (name)", "Potential Name should be unique!")]
-
-    name = fields.Char(string="Potential")
